4934a650f12bac2bbfa6fb79f8464a7ce1de3036
LCY0502 <cyl027@ucsd.edu>
2025-05-17 19:35:39 -0700

    create card.js + template for render:
    have a simple css for rendering card.

---
+++ src/card/triton-card.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+class TritonCard extends HTMLElement{
+	constructor() {
+		super(); // Inherit everything from HTMLElement
+		this.attachShadow( { mode: "open" } );  // Attach a shadowDom to this component
+
+		const article = document.createElement("article")
+		
+		const style = document.createElement("style");
+		
+  }
+};
