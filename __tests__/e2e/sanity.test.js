49882261930fb3a461214d23b943b3fcb6dc6d04
Pranay Jha <pranay.jha@gmail.com>
2025-05-25 04:45:14 -0700

    try

--- __tests__/e2e/sanity.test.js
+++ __tests__/e2e/sanity.test.js
@@ -1,27 +1,13 @@
-import puppeteer from "puppeteer";
-
-describe("Sanity check for game-lobby.html", () => {
-  let browser;
-  let page;
-
+describe('Game Lobby sanity check', () => {
   beforeAll(async () => {
-    browser = await puppeteer.launch({ headless: true });
-    page = await browser.newPage();
+    await page.goto('http://localhost:8080/game-lobby.html');
   });
 
-  afterAll(async () => {
-    await browser.close();
+  it('loads with correct title', async () => {
+    expect(await page.title()).toBe('Document');
   });
 
-  it("loads the game lobby page and has correct title", async () => {
-    await page.goto("http://localhost:8080/game-lobby.html");
-    const title = await page.title();
-    expect(title).toBe("Document");
-  });
-
-  it("finds the Card-Jitsu button on the page", async () => {
-    await page.goto("http://localhost:8080/game-lobby.html");
-    const button = await page.$("#card-jitsu");
-    expect(button).toBeTruthy();
+  it('finds the Card-Jitsu button', async () => {
+    expect(await page.$('#card-jitsu')).toBeTruthy();
   });
 });
