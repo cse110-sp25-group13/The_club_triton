74b8c81c60fbfb7c197af48a789c37276255978b
Pranay Jha <pranay.jha@gmail.com>
2025-05-25 01:37:33 -0700

    test dir reorg; added cov dir to gitignore

--- __tests__/unit/index.test.js
+++ __tests__/unit/index.test.js
@@ -2,13 +2,13 @@ import { readFile } from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import { dummy } from "../src/scripts/script.js";
+import { dummy } from "../../src/scripts/script.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 test("index.html is not empty", async () => {
-  const filePath = path.join(__dirname, "../src/pages/index.html");
+  const filePath = path.join(__dirname, "../../src/pages/index.html");
   const content = (await readFile(filePath, "utf-8")).trim();
   expect(content.length).toBeGreaterThan(0);
 });
