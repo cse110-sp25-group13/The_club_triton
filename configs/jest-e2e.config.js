1607b2a71d1dcaabc9bc351eb439fdd2b2d16f36
Kate Stadler <khs-git@stadfam.net>
2025-05-25 18:27:30 -0700

    fix global setup

--- configs/jest-e2e.config.js
+++ configs/jest-e2e.config.js
@@ -2,7 +2,7 @@ export default {
   preset: "jest-puppeteer",
   testMatch: ["**/__tests__/e2e/**/*.test.js"],
   verbose: true,
-  globalSetup: "<rootDir>/__tests__/setup/globalSetup.js",
-  globalTeardown: "<rootDir>/__tests__/setup/globalTeardown.js",
+  globalSetup: "../__tests__/setup/globalSetup.js",
+  globalTeardown: "../__tests__/setup/globalTeardown.js",
   maxWorkers: 1,
 };
