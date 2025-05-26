f050f8f317c5b736ce79b5d332e2ba969f3066ad
Kate Stadler <khs-git@stadfam.net>
2025-05-25 18:35:33 -0700

    fix path for testMatch for jest e2e

--- configs/jest-e2e.config.js
+++ configs/jest-e2e.config.js
@@ -1,6 +1,6 @@
 export default {
   preset: "jest-puppeteer",
-  testMatch: ["**/__tests__/e2e/**/*.test.js"],
+  testMatch: ["../__tests__/e2e/**/*.test.js"],
   verbose: true,
   globalSetup: "../__tests__/setup/globalSetup.js",
   globalTeardown: "../__tests__/setup/globalTeardown.js",
