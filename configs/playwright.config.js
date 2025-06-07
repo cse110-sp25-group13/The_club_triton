36a6bec51fdb18dde139157acf6388409d7f4a67
Pranay Jha <pranay.jha@gmail.com>
2025-06-07 05:13:39 -0700

    microsoft troll

--- configs/playwright.config.js
+++ configs/playwright.config.js
@@ -1,26 +1,26 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: '../__tests__/browser',
+  testDir: "../__tests__/browser",
   use: {
     headless: true,
-    baseURL: 'https://cse110-sp25-group13.github.io/The_club_triton/src/pages/',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    baseURL: "https://cse110-sp25-group13.github.io/The_club_triton/src/pages/",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
   projects: [
     {
-      name: 'chromium',
-      use: { browserName: 'chromium' },
+      name: "chromium",
+      use: { browserName: "chromium" },
     },
     {
-      name: 'firefox',
-      use: { browserName: 'firefox' },
+      name: "firefox",
+      use: { browserName: "firefox" },
     },
     /*{
       name: 'webkit',
       use: { browserName: 'webkit' },
     },*/
   ],
-  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  reporter: [["html", { outputFolder: "playwright-report" }]],
 });
