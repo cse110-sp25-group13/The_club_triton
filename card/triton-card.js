418294957cdbde943fc71df4e7607305d4d7f093
kstad21 <120453937+kstad21@users.noreply.github.com>
2025-06-08 17:12:10 -0700

    Update triton-card.js

--- card/triton-card.js
+++ card/triton-card.js
@@ -298,10 +298,8 @@ class TritonCard extends HTMLElement {
     const img = this.#card.querySelector("#img-card-front");
     if (img) {
       if (src) {
-        // If src starts with '/src/', it's a project-relative path from cards.json - add basePath
-        // Otherwise, treat as already properly formatted URL or relative path
-        img.src = src.startsWith("/assets/")
-          ? `${TRITON_CARD_BASE_PATH}${src}`
+        img.src = src.startsWith("/src/")
+          ? `${TRITON_CARD_BASE_PATH}${src.replace(/^\/src\//, "/assets/")}`
           : src;
         img.alt = "Card Front";
       } else {
@@ -320,10 +318,8 @@ class TritonCard extends HTMLElement {
     const img = this.#card.querySelector("#img-card-back");
     if (img) {
       if (src) {
-        // If src starts with '/src/', it's a project-relative path from cards.json - add basePath
-        // Otherwise, treat as already properly formatted URL or relative path
-        img.src = src.startsWith("/assets/")
-          ? `${TRITON_CARD_BASE_PATH}${src}`
+        img.src = src.startsWith("/src/")
+          ? `${TRITON_CARD_BASE_PATH}${src.replace(/^\/src\//, "/assets/")}`
           : src;
         img.alt = "Card Back";
       } else {
